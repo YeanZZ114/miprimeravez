e9a923106748d92a0201c83bf9c7f71b4c90b77e
“YeanZZ114” <189868447+YeanZZ114@users.noreply.github.com.>
2024-12-06 08:59:57 -0500

    ......

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-print("Hello World") 
-#VARIABLES
-#TIPOS DE DATOS PRIMITIVOS
-#-STRING = CADENAS DE CARACTERES 'STR'
-#-CHAR = CARACRTER 'STR'
-#-NUMBER = ENTEROS 'INT', DECIMAL 'FLOAT'
-#-BOLEAN = DATOS LOGICOS 'BOOL'
+from design.menu import design
 
-name="Jean Paul"
-age= 18
-height = 1.75
-trainer =False
-#conjuntos de datos
-#-array = conjuntos de datos de una dimencion 'list'
-hobbies = list()
-hobbies =["cocinar","programar","dormir","musica"]
-#modificaar un dato de una lista
-#hobbies[2] = "despertarme"
-
-#solicitar datos en la consola y guardarlos en la lista
-#dato = input("ingrese otro pasatiempos:  ")
-#hobbies.append(dato)
-#print(hobbies)
-
-#eliminar el ultimo dato de la lista
-hobbies.pop()
-print(hobbies)
-
+while True:
+    opc = design()
+    if opc == 0: break
